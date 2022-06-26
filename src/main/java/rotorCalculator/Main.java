aa907aa8b658c3d36e9d0ecc49a1498ff3d0681b
Oskars AA <66279792+Krumuvecis@users.noreply.github.com>
2022-06-26 21:45:24 +0300

    basics

---
+++ src/main/java/rotorCalculator/Main.java
@@ -0,0 +1,7 @@
+package rotorCalculator;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is the main class of the rotor calculator");
+    }
+}
