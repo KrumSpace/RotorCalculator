d20a680a30ef07c7d45a4271a37d3a349056be77
Oskars AA <66279792+Krumuvecis@users.noreply.github.com>
2022-06-27 02:36:19 +0300

    completion

--- src/main/java/preciseUnits/dimensions/primaries/temperature/Temperature.java
+++ src/main/java/preciseUnits/dimensions/primaries/temperature/Temperature.java
@@ -1,10 +1,11 @@
 package preciseUnits.dimensions.primaries.temperature;
 
 import preciseUnits.dimensions.primaries.AbstractPrimaryDimension;
+import preciseUnits.dimensions.primaries.temperature.units.Kelvin;
 
 //TODO: add javadoc
 public class Temperature extends AbstractPrimaryDimension {
     public Temperature() {
-        super("temperature", null);
+        super("temperature", new Kelvin());
     }
 }
--- src/main/java/preciseUnits/dimensions/primaries/time/Time.java
+++ src/main/java/preciseUnits/dimensions/primaries/time/Time.java
@@ -1,10 +1,11 @@
 package preciseUnits.dimensions.primaries.time;
 
 import preciseUnits.dimensions.primaries.AbstractPrimaryDimension;
+import preciseUnits.dimensions.primaries.time.units.Second;
 
 //TODO: add javadoc
 public class Time extends AbstractPrimaryDimension {
     public Time() {
-        super("time", null);
+        super("time", new Second());
     }
 }
